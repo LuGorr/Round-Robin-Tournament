342f9d5b47e8104f3a61e2482e82fcea613b63ce
Ludovico Gorrieri <125456206+LuGorr@users.noreply.github.com>
2025-05-27 17:16:49 +0200

    Update check.py
    added comments

--- check.py
+++ check.py
@@ -1,8 +1,11 @@
 import json
 import os
 import ast
+#Get path (needs to be more general, maybe cli input)
 path = os.getcwd() + "/CP/test.json"
 
+#Open json
 with open(path, 'r') as file:
     data = json.load(file)
+#Loads the results in a matrix (needs to be more general, if the ouput of another method differs it doesn't work)
 sols = ast.literal_eval(data["output"]["default"])
