86f1d7829efe71d072796d3a00f4c9356a923204
Ludovico Gorrieri <ludovico.gorrieri@gmail.com>
2025-05-28 17:18:13 +0200

    fixed bugs in check.py

--- check.py
+++ check.py
@@ -15,12 +15,25 @@ import copy
 def check(home, away, err):
     #Count occurrences of matches between teams
     occurrences = {}
+    for i in range(len(home)+1):
+        for j in range(len(home)+1):
+            if(j!= i):
+                if (i>=j):
+                    key = f"{i+1}, {j+1}"
+                else:
+                    key = f"{j+1}, {i+1}"
+                occurrences[key] = 0
+    
     teams_per_week = [] 
     for i in range(len(home)):
         teams_per_week.append(sorted(home[i]+away[i]))
         for j in range(len(home[0])):
-            key = f"{home[i][j]}, {away[i][j]}"
-            occurrences[key] = occurrences.get(key, 0) + 1
+            if (home[i][j]>=away[i][j]):
+                key = f"{home[i][j]}, {away[i][j]}"
+            else:
+                key = f"{away[i][j]}, {home[i][j]}"
+            occurrences[key] += 1
+
     #Count occurrences of teams during a period
     period_occurrences = {}
     for j in range(len(home[0])):
@@ -29,26 +42,29 @@ def check(home, away, err):
             key2 = f"{j}, {away[i][j]}"
             period_occurrences[key1] = period_occurrences.get(key1, 0) + 1
             period_occurrences[key2] = period_occurrences.get(key2, 0) + 1
-
+    err_period = False
     for i in range(len(home)):
-        if teams_per_week[i] == list(range(len(home))):
-            print(f"Not every team has played during week {i+1}")
+        if teams_per_week[i] != list(range(1,len(home)+2)):
+            print(f"Not every team has played during week {i+1}, {teams_per_week[i]}")
             err = True
         for j in range(len(home[0])):
-            key = f"{home[i][j]}, {away[i][j]}"
             key1 = f"{j}, {home[i][j]}"
             key2 = f"{j}, {away[i][j]}"
-            #Check if a matchup has happened more than one time or if it never took place
-            if(occurrences[key] > 1):
-                print(f"More than a match between team {home[i][j]} and {away[i][j]}")
+            if err_period:
+                break
+            if((period_occurrences[key1] > 2) or (period_occurrences[key2] > 2)):
+                print(f"At least a team has played more than two matches during the same period")
                 err = True
-            if(occurrences[key] < 1):
-                print(f"No match between team {home[i][j]} and {away[i][j]}")
+                err_period = True
+    for key, elem in occurrences.items():
+        #Check if a matchup has happened more than one time or if it never took place
+            if(elem > 1):
+                print(f"More than a match between team {key[0]} and {key[3]}")
+                err = True
+            if(elem < 1):
+                print(f"No match between team {key[0]} and {key[3]}")
                 err = True
             #Check if a team has played more than two times over the same period
-            if((period_occurrences[key1] > 2) or (period_occurrences[key2] > 2)):
-                print("At least a team has played more than two matches during the same period")
-                err = True
     return err
 #Get path (needs to be more general, maybe cli input)
 path = os.getcwd() + "/CP/test.json"
@@ -70,10 +86,11 @@ for i in range(len(home)):
     err.append(check(home[i], away[i], False))
 
 
-
-for error in err:
+global_error = False
+for i, error in enumerate(err):
     if error:
-        print("Check terminated with errors!")
-        exit
+        print(f"Check n. {i+1} terminated with errors!")
+        global_error = True
 
-print("Check terminated successfully")
+if not global_error:
+    print("Check terminated successfully")
