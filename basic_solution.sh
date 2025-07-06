6e0723f5f8a7ff4fb25bce83409d7fa8c552db35
Ludovico Gorrieri <ludovico.gorrieri@gmail.com>
2025-07-06 19:37:46 +0200

    pushed everything

---
+++ basic_solution.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+while getopts "n:s:" opt; do
+  case $opt in
+    n)
+      n=$OPTARG
+      ;;
+    s)
+      s=$OPTARG
+      ;;
+    *)
+      echo "Opzione non valida"
+      exit 1
+      ;;
+  esac
+done
+
+json=$(minizinc --disable-warnings --statistics --json-stream --param-file $s --solver-time-limit 300000 CP/basic_solution.mzn -D "n=$n")
+
+if [ "$(echo "$json" | jq -r 'select(.type == "status") | .status')" == "UNSATISFIABLE" ]; then
+  formatted_sol="[]";
+  optimal="true";
+  if [ "$(echo $s | grep "coinbc")" != "" ]; then
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null" | head -n 1)
+  else
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null")
+  fi
+elif [ "$(echo "$json" | jq -r 'select(.type == "status") | .status')" == "UNKNOWN" ]; then
+  formatted_sol="[]";
+  solve="300";
+  optimal="false";
+else
+  optimal="false"
+  formatted_sol=$(echo $json | jq   '.output.default | select(. != null)');
+  formatted_sol="${formatted_sol#\"}";
+  formatted_sol="${formatted_sol%\"}";
+  if [ "$(echo $s | grep "coinbc")" != "" ]; then
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null" | head -n 1)
+  else
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null")
+  fi 
+fi
+
+file_path="../../res/CP/"$n".json"
+solver="${s#solver-configs/}"
+solver="${solver%.mpc}"
+
+if [  -s $file_path ]; then 
+truncate -s -2 $file_path; 
+echo ",">> $file_path; else
+echo "{">> $file_path;  fi
+echo '    "basic-solution-'"$solver"-n-"$n\":{" >> $file_path
+echo '        "time":'$(echo "scale=0; ("$solve")/1" | bc )','>> $file_path
+echo '        "optimal":'$optimal','>> $file_path
+echo '        "obj": "None",'>> $file_path
+echo '        "sol": '$formatted_sol>> $file_path
+echo '    }'>> $file_path
+echo '}'>> $file_path
--- optimization.sh
+++ optimization.sh
@@ -15,21 +15,65 @@ while getopts "n:s:" opt; do
   esac
 done
 #json=$(minizinc --statistics --json-stream --solver-time-limit 300000 --param-file $s CP/optimization.mzn -D "n=$n")
-json=$(minizinc --statistics --json-stream --param-file $s CP/optimization.mzn -D "n=$n")
-echo $json > "CP/test.json"
+json=$(minizinc --disable-warnings --statistics --json-stream --param-file $s --solver-time-limit 300000 CP/optimization.mzn -D "n=$n")
 
-echo "solveTime: $(jq ".statistics.solveTime" "CP/test.json" | grep -v "null")"
-objectiveValue=$((echo $json | jq -r ".output.default | select(. != null)") | 
-awk '{
-dollar = index($0, "$")
 
-if(dollar == 0){
-next
-}
-obj = substr($0, dollar+2 )
-bracket = index(obj, "]")
-print substr(obj,0,bracket -1)
-}')
-echo "objectiveValue: $objectiveValue"
+if [ "$(echo "$json" | jq -r 'select(.type == "status") | .status')" == "UNSATISFIABLE" ]; then
+  formatted_sol="[]";
+  optimal="true";
+  if [ "$(echo $s | grep "coinbc")" != "" -o "$(echo $s | grep "ortools")" != "" ]; then
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null" | head -n 1)
+  else
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null")
+  fi
+  objectiveValue="None"
+elif [ "$(echo "$json" | jq -r 'select(.type == "status") | .status')" == "UNKNOWN" ]; then
+  formatted_sol="[]";
+  solve="300";
+  optimal="false";
+  objectiveValue='"None"'
+else
+  optimal="true"
+  #formatted_sol=$(echo $json | jq   '.output.default | select(. != null)');
+  #formatted_sol="${formatted_sol#\"}";
+  #formatted_sol="${formatted_sol%\"}";
+  if [ "$(echo $s | grep "coinbc")" != "" -o "$(echo $s | grep "ortools")" != "" ]; then
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null" | head -n 1)
+  else
+    
+    solve=$(echo $json | jq -r ".statistics.solveTime"| grep -v "null")
+    
+  fi
+
+  formatted_sol=$(echo $json | jq -r ".output.default | select(. != null)" | awk '{
+  bracket_open = index($0, "[")
+  dollar = index($0, "$")
+  print(substr($0, bracket_open, dollar-1))}')
+  objectiveValue=$((echo $json | jq -r ".output.default | select(. != null)") | 
+  awk '{
+  dollar = index($0, "$")
+
+  if(dollar == 0){
+  next
+  }
+  obj = substr($0, dollar+2 )
+  bracket = index(obj, "]")
+  print substr(obj,0,bracket -1)
+  }'); fi
+
+file_path="../../res/CP/"$n".json"
+solver="${s#solver-configs/}"
+solver="${solver%.mpc}"
+if [  -s $file_path ]; then 
+truncate -s -2 $file_path; 
+echo ",">> $file_path; else
+echo "{">> $file_path;  fi
+echo '    "optimal-'"$solver"-n-"$n\":{" >> $file_path
+echo '        "time":'$(echo "scale=0; ("$solve")/1" | bc )','>> $file_path
+echo '        "optimal":'$optimal','>> $file_path
+echo '        "obj":'$objectiveValue','>> $file_path
+echo '        "sol": '$formatted_sol>> $file_path
+echo '    }'>> $file_path
+echo '}'>> $file_path
 
 
